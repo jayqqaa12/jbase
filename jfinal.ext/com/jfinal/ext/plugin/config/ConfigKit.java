a9c96ab87f3e8209915fa32850c7c9beeb24df33
476335667@qq.com <476335667@qq.com>
2016-01-27 17:16:45 +0800

    添加configKit 设置开发模式

--- jfinal.ext/com/jfinal/ext/plugin/config/ConfigKit.java
+++ jfinal.ext/com/jfinal/ext/plugin/config/ConfigKit.java
@@ -29,129 +29,140 @@ import com.jfinal.log.Log;
 
 public class ConfigKit {
 
-    protected final static Log LOG = Log.getLog(ConfigKit.class);
+	protected final static Log LOG = Log.getLog(ConfigKit.class);
 
-    private static List<String> includeResources;
+	private static List<String> includeResources;
 
-    private static List<String> excludeResources;
+	private static List<String> excludeResources;
 
-    private static Map<String, String> map = Maps.newHashMap();
+	private static Map<String, String> map = Maps.newHashMap();
 
-    private static Map<String, String> testMap = Maps.newHashMap();
+	private static Map<String, String> testMap = Maps.newHashMap();
 
-    private static Map<String, Long> lastmodifies = Maps.newHashMap();
+	private static Map<String, Long> lastmodifies = Maps.newHashMap();
 
-    private static boolean reload = true;
+	private static boolean reload = true;
 
-    /**
-     * @param includeResources
-     * @param excludeResources
-     * @param reload
-     */
-    static void init(List<String> includeResources, List<String> excludeResources, boolean reload) {
-        ConfigKit.includeResources = includeResources;
-        ConfigKit.excludeResources = excludeResources;
-        ConfigKit.reload = reload;
-        for (final String resource : includeResources) {
-            LOG.debug("include :" + resource);
-            File[] propertiesFiles = null;
-            propertiesFiles = new File(PathKit.getRootClassPath()).listFiles(new FileFilter() {
+	private static boolean dev = false;
 
-                @Override
-                public boolean accept(File pathname) {
-                    return Pattern.compile(resource).matcher(pathname.getName()).matches();
-                }
-            });
-            for (File file : propertiesFiles) {
-                String fileName = file.getName();
-                LOG.debug("fileName:" + fileName);
-                if (fileName.endsWith("-test." + ConfigPlugin.suffix)) {
-                    continue;
-                }
-                boolean excluded = false;
-                for (String exclude : excludeResources) {
-                    if (Pattern.compile(exclude).matcher(file.getName()).matches()) {
-                        excluded = true;
-                    }
-                }
-                if (excluded) {
-                    continue;
-                }
-                lastmodifies.put(fileName, new File(fileName).lastModified());
-                map.putAll(ResourceKit.readProperties(fileName));
-                try {
-                    testMap.putAll(ResourceKit.readProperties(testFileName(fileName)));
-                } catch (IllegalArgumentException e) {
-                    LOG.info(e.getMessage());
-                }
-            }
-        }
-        LOG.debug("map" + map);
-        LOG.debug("testMap" + testMap);
-        LOG.info("config init success!");
-    }
+	public void setDev(boolean dev) {
+		this.dev = dev;
+	}
 
-    private static String testFileName(String fileName) {
-        return fileName.substring(0, fileName.indexOf("." + ConfigPlugin.suffix)) + "-test." + ConfigPlugin.suffix;
-    }
+	public boolean getDev() {
+		return dev;
+	}
 
-    public static String getStr(String key, String defaultVal) {
-        if (testMap == null || map == null) {
-            throw new RuntimeException(" please start ConfigPlugin first~");
-        }
-        if (reload) {
-            checkFileModify();
-        }
-        String val = testMap.get(key);
-        if (val == null || "".equals(val.trim())) {
-            val = map.get(key);
-        }
-        return val == null ? defaultVal : val + "";
+	/**
+	 * @param includeResources
+	 * @param excludeResources
+	 * @param reload
+	 */
+	static void init(List<String> includeResources, List<String> excludeResources, boolean reload) {
+		ConfigKit.includeResources = includeResources;
+		ConfigKit.excludeResources = excludeResources;
+		ConfigKit.reload = reload;
+		for (final String resource : includeResources) {
+			LOG.debug("include :" + resource);
+			File[] propertiesFiles = null;
+			propertiesFiles = new File(PathKit.getRootClassPath()).listFiles(new FileFilter() {
 
-    }
+				@Override
+				public boolean accept(File pathname) {
+					return Pattern.compile(resource).matcher(pathname.getName()).matches();
+				}
+			});
+			for (File file : propertiesFiles) {
+				String fileName = file.getName();
+				LOG.debug("fileName:" + fileName);
+				if (fileName.endsWith("-test." + ConfigPlugin.suffix)) {
+					continue;
+				}
+				boolean excluded = false;
+				for (String exclude : excludeResources) {
+					if (Pattern.compile(exclude).matcher(file.getName()).matches()) {
+						excluded = true;
+					}
+				}
+				if (excluded) {
+					continue;
+				}
+				lastmodifies.put(fileName, new File(fileName).lastModified());
+				map.putAll(ResourceKit.readProperties(fileName));
 
-    private static void checkFileModify() {
-        Set<String> filenames = lastmodifies.keySet();
-        for (String filename : filenames) {
-            File file = new File(filename);
-            if (lastmodifies.get(filename) != file.lastModified()) {
-                LOG.info(filename + " changed, reload.");
-                init(includeResources, excludeResources, reload);
-            }
-        }
-    }
+				try {
+					if (dev) testMap.putAll(ResourceKit.readProperties(testFileName(fileName)));//12 add
+				} catch (IllegalArgumentException e) {
+					LOG.info(e.getMessage());
+				}
+			}
+		}
+		LOG.debug("map" + map);
+		LOG.debug("testMap" + testMap);
+		LOG.info("config init success!");
+	}
 
-    public static String getStr(String key) {
-        return getStr(key, "");
-    }
+	private static String testFileName(String fileName) {
+		return fileName.substring(0, fileName.indexOf("." + ConfigPlugin.suffix)) + "-test." + ConfigPlugin.suffix;
+	}
 
-    public static long getLong(String key) {
-        return getLong(key, 0);
-    }
+	public static String getStr(String key, String defaultVal) {
+		if (testMap == null || map == null) {
+			throw new RuntimeException(" please start ConfigPlugin first~");
+		}
+		if (reload) {
+			checkFileModify();
+		}
+		String val = testMap.get(key);
+		if (val == null || "".equals(val.trim())) {
+			val = map.get(key);
+		}
+		return val == null ? defaultVal : val + "";
 
-    public static long getLong(String key, long defaultVal) {
-        String val = getStr(key).trim();
-        if ("".equals(val)) {
-            return defaultVal;
-        }
-        return Long.parseLong(val);
-    }
+	}
 
-    public static int getInt(String key, int defaultVal) {
-        String val = getStr(key).trim();
-        if ("".equals(val)) {
-            return defaultVal;
-        }
-        return Integer.parseInt(val);
-    }
+	private static void checkFileModify() {
+		Set<String> filenames = lastmodifies.keySet();
+		for (String filename : filenames) {
+			File file = new File(filename);
+			if (lastmodifies.get(filename) != file.lastModified()) {
+				LOG.info(filename + " changed, reload.");
+				init(includeResources, excludeResources, reload);
+			}
+		}
+	}
 
-    public static int getInt(String key) {
-        return getInt(key, 0);
-    }
+	public static String getStr(String key) {
+		return getStr(key, "");
+	}
 
-    public static void set(String key, String val) {
-        // TODO
-        throw new RuntimeException("I do not know how to do it now..");
-    }
+	public static long getLong(String key) {
+		return getLong(key, 0);
+	}
+
+	public static long getLong(String key, long defaultVal) {
+		String val = getStr(key).trim();
+		if ("".equals(val)) {
+			return defaultVal;
+		}
+		return Long.parseLong(val);
+	}
+
+	public static int getInt(String key, int defaultVal) {
+		String val = getStr(key).trim();
+		if ("".equals(val)) {
+			return defaultVal;
+		}
+		return Integer.parseInt(val);
+	}
+
+	public static int getInt(String key) {
+		return getInt(key, 0);
+	}
+
+	public static void set(String key, String val) {
+		// TODO
+		throw new RuntimeException("I do not know how to do it now..");
+	}
 
 }

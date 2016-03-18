5c36a8c5c7de5209ccb6a1ceede36f4971c63ba2
476335667@qq.com <476335667@qq.com>
2016-03-18 11:12:12 +0800

    1

--- src/main/java/com/jayqqaa12/jbase/jfinal/ext/JsonValidator.java
+++ src/main/java/com/jayqqaa12/jbase/jfinal/ext/JsonValidator.java
@@ -5,40 +5,37 @@ import com.jayqqaa12.model.json.SendJson;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.StrKit;
 
-public abstract class JsonValidator extends Validator
-{
+public abstract class JsonValidator extends Validator {
 	protected static final String ERROR_MSG = "code";
 
-	protected void addError(int code )
-	{
-		super.addError("code", code+"");
+	protected void addError(int code) {
+		super.addError("code", code + "");
 	}
-	
+
 	protected void validateRequiredString(String field, int errorCode) {
 		String value = controller.getPara(field);
-		if (value == null || "".equals(value.trim()))
-			addError(errorCode);
+		if (value == null || "".equals(value.trim())) addError(errorCode);
 	}
-	protected void validateString(String field, int minLen, int maxLen, int errorCode)
-	{
+
+	protected void validateString(String field, int minLen, int maxLen, int errorCode) {
 		validateStringValue(controller.getPara(field), minLen, maxLen, errorCode);
 	}
- 
+
 	private void validateStringValue(String value, int minLen, int maxLen, int errorCode) {
 		if (StrKit.isBlank(value)) {
 			addError(errorCode);
-			return ;
+			return;
 		}
-		if (value.length() < minLen || value.length() > maxLen)
-			addError(errorCode);
-	}
-	
-	@Override
-	protected void handleError(Controller c)
-	{
-		c.renderJson(new SendJson(Integer.parseInt((String)c.getAttr("code"))) .toJson());
-		
+		if (value.length() < minLen || value.length() > maxLen) addError(errorCode);
 	}
 
- 
+	@Override
+	protected void handleError(Controller c) {
+
+		String code = c.getAttr("code");
+
+		if (StrKit.notBlank(code)) c.renderJson(new SendJson(Integer.parseInt(code)).toJson());
+
+	}
+
 }
